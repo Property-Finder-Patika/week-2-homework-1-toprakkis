40b10d88bda772406956326feff6aedd19896713
toprakkis <93645179+toprakkis@users.noreply.github.com>
2022-06-30 11:57:14 +0200

    Create naked_expression.go

---
+++ learngo/solution_4/naked_expression.go
@@ -0,0 +1,8 @@
+// In this example, we basically see that expressions without suitable functions, methods etc. are problematic syntax-wise
+package main
+
+import "fmt"
+
+func main(){
+ "Hello" 
+}
