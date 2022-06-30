bdf1faa408670c5f5e071cbc5c3e2954173c5273
toprakkis <93645179+toprakkis@users.noreply.github.com>
2022-06-30 12:10:05 +0200

    Create main.go

---
+++ learngo/solution_5/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt" 
+
+	"https://github.com/Property-Finder-Patika/week-2-homework-1-toprakkis/new/main/learngo/solution_5"
+)
+
+func main() {
+	fmt.Println(golang.Version())
+}
